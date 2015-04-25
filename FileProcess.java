c9a14ad901c3d007a08afa7a68537a99e1dc9e34
seanliuxianlong <xianlongliu@hotmail.com>
2015-04-25 20:53:56 +0800

    first commit

---
+++ FileProcess.java
@@ -0,0 +1,109 @@
+package filehandle;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+/*
+ * The class is to read file and operate the float number in the file, get total summary for those data and 
+ * @author SeanLiu
+ * @date  April 25, 2015
+ */
+public class FileProcess {
+	/*
+	 * The method is to Delete those space which is more than second space
+	 * 
+	 * @parameter String orginalString
+	 * @return String    formatString
+	 */
+    public static String DeleteMoreSpace(String originalString){
+        String formatString = "";	 
+    	for (int i = 0; i < originalString.length() - 1; i++) {
+            if ((int) originalString.charAt(i) == 32 && (int) originalString.charAt(i + 1) == 32) {
+    	    continue;
+    	   }
+    	   formatString += originalString.charAt(i);
+    	}
+    	if ((int) originalString.charAt(originalString.length() - 1) != 32)
+    	    formatString += originalString.charAt(originalString.length() - 1);
+    	    System.out.println("Formatted without more than two space string"+formatString);
+    	 return formatString;
+     }
+    
+     /*
+      * The method is to convert String[] to float and also calculate the summary of all. 
+      *
+      * @parameter String[] arrays
+	  * @return float
+      * 
+      */
+    public static float Sum(String[] arrays) throws NumberFormatException {
+        float subTotal=0;
+    	try{
+            for(int i=0;i<arrays.length; i++){
+            	System.out.println("original number is"+arrays[i]);
+                subTotal +=Float.parseFloat(arrays[i]);
+                System.out.println("subtotal is"+arrays[i]);
+            }
+    	}
+        catch(NumberFormatException e){
+        	throw e;
+        	
+        }
+        return subTotal;
+     }
+    /*
+     * handler the file which is specified. 
+     * @parameter String path The path of the file which is specified.
+     */
+    public static void handlerFile(String path){
+    	try {
+            // read file content from file
+            FileReader reader = new FileReader(path);
+            System.out.println("The file you want to processw is"+path);
+        
+            BufferedReader br = new BufferedReader(reader);
+            String str = null;
+            int count = 0;
+            float total = 0;
+            while((str = br.readLine()) != null) {
+       	    //read one line and remove more space and convert them to String array
+            String [] stringArrays = DeleteMoreSpace(str).split(" ");
+            //calculate total summary of the stringArrays
+            total += Sum(stringArrays);
+            //calculate total number count of stringArrays
+            count +=stringArrays.length;
+            }
+            System.out.println("Total count of those floats is"+count);
+            System.out.println("Total summary of those floats is"+total);
+            br.close();
+            reader.close();
+        }
+        catch(FileNotFoundException e) {
+              e.printStackTrace();
+        }
+        catch(IOException e) {
+              e.printStackTrace();
+        }
+        catch(NumberFormatException e) {
+            e.printStackTrace();
+        }	
+    }
+
+    
+	public static void main(String[] args) {
+        if (args.length==0){
+            System.out.println("Please input your file name");
+        }
+         
+        else{
+        	//test cases1, it is a normal case
+        	handlerFile(args[0]);
+        	//test cases2, include abnormal float. e.g. append English character, long float  
+        	//handlerFile(args[0]);
+        	//test cases3, include an empty file without any data
+        }         
+    }
+}
